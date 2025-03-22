0a11a0d0dd2b05fa9d567621b1310f3152814925
Dasun Dilshan <164143926+kmddkuruppu@users.noreply.github.com>
2025-03-22 20:43:06 +0530

    Update

---
+++ BACKEND/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const path = require("path");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8070;
+const URL = process.env.MONGODB_URL;
+
+if (!URL) {
+    console.error("MongoDB URL is not defined in the .env file");
+    process.exit(1); // Exit the process if MongoDB URL is not defined
+}
+
+console.log("MongoDB URL:", URL); // For debugging
+
+app.use('/uploads', express.static  (path.join(__dirname, 'uploads')));
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch(error => {
+    console.error("MongoDB Connection Error:", error);
+    process.exit(1); // Exit the process if there is an error connecting to MongoDB
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB connection success");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
