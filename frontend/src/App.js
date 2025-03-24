57ebc2fa67805fed4fc3fbbbb0ed3ad3d3b658d1
Dasun Dilshan <164143926+kmddkuruppu@users.noreply.github.com>
2025-03-24 21:17:03 +0530

    update budget

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+//import components
+import ExpenseForm from './components/ExpenseForm';
+import Navbar from './components/Navbar';
+import BudgetOverview from './components/BudgetOverview';
+import UpdateExpenseForm from './components/UpdateExpenseForm';
+
+//import pages
+import Budgeting from './pages/Budgeting'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          
+          <Route path="/budgeting" element={<Budgeting />} />
+          <Route path="/EXpenseForm" element={<ExpenseForm/>}/>
+          <Route path="/Navbar" element={<Navbar/>}/>
+          <Route path="/display" element={<BudgetOverview />} />
+          <Route path="/update/:expenseId" element={<UpdateExpenseForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
