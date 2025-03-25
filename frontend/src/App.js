e614fbe545c26ccb2867a607d0cc2f1036281830
Dasun Dilshan <164143926+kmddkuruppu@users.noreply.github.com>
2025-03-25 10:34:45 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Budget from './pages/Budget';
-import ExpenseForm from './components/ExpenseForm';
 import Navbar from './components/Navbar';
-import BudgetOverview from './components/BudgetOverview';
-import UpdateExpenseForm from './components/UpdateExpenseForm';
+import Budget from './pages/Budget';
+// import ExpenseForm from './components/ExpenseForm';
+// import BudgetOverview from './components/BudgetOverview';
+// import UpdateExpenseForm from './components/UpdateExpenseForm';
 
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <Routes>
-          
           <Route path="/budget" element={<Budget />} />
-          <Route path="/EXpenseForm" element={<ExpenseForm/>}/>
-          <Route path="/Navbar" element={<Navbar/>}/>
+          {/* <Route path="/expense-form" element={<ExpenseForm />} />
           <Route path="/display" element={<BudgetOverview />} />
-          <Route path="/updateExpense/:expenseId" element={<UpdateExpenseForm />} />
+          <Route path="/updateExpense/:expenseId" element={<UpdateExpenseForm />} /> */}
         </Routes>
       </div>
     </Router>
   );
 }
+
 
 export default App;
