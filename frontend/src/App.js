00ba820c853b0c9e7198d73e1a55c30c8b49094e
Dasun Dilshan <164143926+kmddkuruppu@users.noreply.github.com>
2025-03-25 09:36:30 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-//import components
-import ExpenseForm from './components/ExpenseForm';
-import Navbar from './components/Navbar';
-import BudgetOverview from './components/BudgetOverview';
-import UpdateExpenseForm from './components/UpdateExpenseForm';
-
 //import pages
-import Budgeting from './pages/Budgeting'
+
 
 function App() {
   return (
@@ -16,11 +10,6 @@ function App() {
       <div className="App">
         <Routes>
           
-          <Route path="/budgeting" element={<Budgeting />} />
-          <Route path="/EXpenseForm" element={<ExpenseForm/>}/>
-          <Route path="/Navbar" element={<Navbar/>}/>
-          <Route path="/display" element={<BudgetOverview />} />
-          <Route path="/update/:expenseId" element={<UpdateExpenseForm />} />
         </Routes>
       </div>
     </Router>
