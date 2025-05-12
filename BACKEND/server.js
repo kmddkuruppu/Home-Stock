7cc75a2e2872089286f0a2a720c4de1a28e3d776
Dasun Dilshan <164143926+kmddkuruppu@users.noreply.github.com>
2025-05-12 22:50:39 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -62,6 +62,9 @@ app.use("/predictivebudgeting", predictiveBudgetingRouter);
 const storeSuggestionRouter = require("./routes/storeSuggestion.js");
 app.use("/storesuggestion", storeSuggestionRouter);
 
+const authRouter = require("./routes/auth.js");
+app.use("/auth", authRouter);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 });
